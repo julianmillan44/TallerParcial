b745a319f27258a5e8883a38310c2f09969f6b9e
julianmillan44 <149098880+julianmillan44@users.noreply.github.com>
2024-06-30 14:37:18 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,17 @@
+const express = require('express');
 
+const peliculasRouter = require('./peliculas.router');
+const salaRouter = require('./sala.router');
+const filaRouter = require('./fila.router');
+
+
+function rutas(tallerParcial){
+    const router = express.Router();
+    tallerParcial.use('/', router)
+    
+    router.use('/peliculas', peliculasRouter)
+    router.use('/sala', salaRouter)
+    router.use('/fila', filaRouter)
+}
+
+module.exports = rutas
