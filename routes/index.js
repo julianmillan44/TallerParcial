fd2d0e6eb091c02a4e81e4ae072816010b38c925
julianmillan44 <149098880+julianmillan44@users.noreply.github.com>
2024-06-30 15:06:18 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const peliculasRouter = require('./peliculas.router');
 const salaRouter = require('./sala.router');
 const filaRouter = require('./fila.router');
+const puestosRouter = require('./puestos.router');
+const detalleVentaRouter = require('./detalleVenta.router');
+
 
 
 function rutas(tallerParcial){
@@ -12,6 +15,9 @@ function rutas(tallerParcial){
     router.use('/peliculas', peliculasRouter)
     router.use('/sala', salaRouter)
     router.use('/fila', filaRouter)
+    router.use('/puestos', puestosRouter)
+    router.use('/detalleVenta', detalleVentaRouter)
+    
 }
 
 module.exports = rutas
